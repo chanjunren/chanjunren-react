49cf117f09bc0b755465cf5a2e8925a007f1ecd3
chanjunren <chanjunren@gmail.com>
2021-07-06 17:45:07 +0800

    Fixed styling bug

---
+++ frontend/src/company_site/components/rs_applications/application_data.js
@@ -0,0 +1,28 @@
+import temiPatrolIcon from '../../images/app_logos/temi_patrol_icon.png';
+import temiConciergeIcon from
+  '../../images/app_logos/temi_ohs_concierge_icon.png';
+import temiMuseumIcon from '../../images/app_logos/temi_museum_icon.png';
+import receptemistIcon from '../../images/app_logos/receptemist_icon.png';
+
+export const applicationsData = [
+  {
+    imageSrc: temiPatrolIcon,
+    title: 'Temi Patrol',
+    description: 'Mask detection application',
+  },
+  {
+    imageSrc: temiConciergeIcon,
+    title: 'Temi OHS Concierge',
+    description: 'Concierge Application for NUS OHS',
+  },
+  {
+    imageSrc: receptemistIcon,
+    title: 'Receptemist',
+    description: 'Receptionist Temi Application',
+  },
+  {
+    imageSrc: temiMuseumIcon,
+    title: 'National Museum Guide',
+    description: 'Tour Guide for National Museum',
+  },
+];
--- frontend/src/company_site/components/rs_applications/index.js
+++ frontend/src/company_site/components/rs_applications/index.js
@@ -1,38 +1,69 @@
 import React from 'react';
-
-import temiPatrolIcon from '../../images/app_logos/temi_patrol_icon.png';
-import temiConciergeIcon from
-  '../../images/app_logos/temi_ohs_concierge_icon.png';
-// import temiMuseumIcon from '../../images/app_logos/temi_museum_icon.png';
-import receptemistIcon from '../../images/app_logos/receptemist_icon.png';
-
+import {applicationsData} from './application_data';
 import './rs_applications.css';
 
+import {Swiper, SwiperSlide} from 'swiper/react';
+// Import Swiper styles
+import 'swiper/swiper.min.css';
+import 'swiper/components/effect-coverflow/effect-coverflow.min.css';
+import 'swiper/components/pagination/pagination.min.css';
+
+import SwiperCore, {Pagination, EffectCoverflow} from 'swiper/core';
+
+SwiperCore.use([Pagination, EffectCoverflow]);
+
 const AppsSection = () => {
+  const applications = applicationsData.map((app, idx) => {
+    return (
+      <SwiperSlide
+        tag="li"
+        key={`slider-slide-${idx}`}>
+        <div className="app-card">
+          <img
+            className="app-icon"
+            src={app.imageSrc}
+            alt={`${app.title}-imageSrc`}/>
+          <h2 className="app-h2">{app.title}</h2>
+          <p className="app-p">{app.description}</p>
+        </div>
+      </SwiperSlide>
+    );
+  });
+
+  // const applications = applicationsData.map((app, idx) => {
+  //   return (
+  //     <SwiperSlide
+  //       tag="li"
+  //       key={`slider-slide-${idx}`}>
+  //       <img
+  //         className="app-icon"
+  //         src={app.imageSrc}
+  //         alt={`${app.title}-imageSrc`}/>
+  //     </SwiperSlide>
+  //   );
+  // });
+
   return (
     <div className="apps-container" id="applications">
       <h1 id="apps-h1">Our Applications</h1>
       <div id="apps-wrapper">
-        <div className="app-card">
-          <img className="app-icon" src={temiPatrolIcon}/>
-          <h2 className="app-h2">Temi Patrol</h2>
-          <p className="app-p">Mask detection application</p>
-        </div>
-        <div className="app-card">
-          <img className="app-icon" src={temiConciergeIcon}/>
-          <h2 className="app-h2">Temi OHS Concierge</h2>
-          <p className="app-p">Concierge Application for NUS OHS</p>
-        </div>
-        <div className="app-card">
-          <img className="app-icon" src={receptemistIcon}/>
-          <h2 className="app-h2">Receptemist</h2>
-          <p className="app-p">Receptionist Temi Application</p>
-        </div>
-        {/* <AppCard>
-          <AppIcon src={temiMuseumIcon}/>
-          <AppH2>National Museum Concierge</AppH2>
-          <AppP>Concierge Application for National Museum</AppP>
-        </AppCard> */}
+        <Swiper id="main"
+          tag="section"
+          wrapperTag="ul"
+          centeredSlides={true}
+          effect={'coverflow'}
+          grabCursor={true}
+          slidesPerView={'auto'}
+          coverflowEffect={{
+            'rotate': 50,
+            'stretch': 0,
+            'depth': 0,
+            'modifier': 1,
+            'slideShadows': false,
+          }}
+          pagination>
+          {applications}
+        </Swiper>
       </div>
     </div>
   );
