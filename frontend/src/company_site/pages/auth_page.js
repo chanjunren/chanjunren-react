79fbd51e5ad7ac0afae1b20211f56734ebcaddef
chanjunren <chanjunren@gmail.com>
2021-07-13 18:36:32 +0800

    Implemented http hook | to be continued

--- frontend/src/company_site/pages/auth_page.js
+++ frontend/src/company_site/pages/auth_page.js
@@ -5,22 +5,31 @@ import avatar from '../images/avatar.png';
 
 import './auth_page.css';
 import useAuth from '../hooks/auth_hook';
+// import useHttpClient from '../hooks/http_hook';
 
 const AuthPage = () => {
-  const [userInputFocused, passInputFocused,
-    focusUserInput, focusPassInput,
-    unFocusUserInput, unFocusPassInput] = useAuth();
+  const [
+    userInputFocused,
+    passInputFocused,
+    focusUserInput,
+    focusPassInput,
+    unFocusUserInput,
+    unFocusPassInput,
+  ] = useAuth();
+
+  // const {isLoading, errorEncountered, sendRequest, clearError} =
+  //   useHttpClient;
   return (
     <>
       <div className="container">
-        <img id='login-display'src={loginDisplay} alt='login-display'/>
+        <img id="login-display" src={loginDisplay} alt="login-display" />
         <div className="login-content">
           <form>
-            <img src={avatar} alt='login-avatar'/>
+            <img src={avatar} alt="login-avatar" />
             <h2 className="title">Welcome</h2>
             <div className={`input-div one ${userInputFocused && 'focus'}`}>
               <div className="icon-holder">
-                <FaUserAlt className="fas fa-user"/>
+                <FaUserAlt className="fas fa-user" />
               </div>
               <div className="div">
                 <h5>Username</h5>
@@ -28,13 +37,13 @@ const AuthPage = () => {
                   type="text"
                   className="input"
                   onFocus={focusUserInput}
-                  onEmptied={unFocusUserInput}
+                  onFocusOut={unFocusUserInput}
                 />
               </div>
             </div>
             <div className={`input-div pass ${passInputFocused && 'focus'}`}>
               <div className="icon-holder">
-                <FaLock className="fas fa-lock"/>
+                <FaLock className="fas fa-lock" />
               </div>
               <div className="div">
                 <h5>Password</h5>
@@ -42,7 +51,7 @@ const AuthPage = () => {
                   type="password"
                   className="input"
                   onFocus={focusPassInput}
-                  onEmptied={unFocusPassInput}
+                  onFocusOut={unFocusPassInput}
                 />
               </div>
             </div>
