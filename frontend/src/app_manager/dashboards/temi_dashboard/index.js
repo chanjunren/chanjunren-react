28f0d2665b0a3eed00b42e2b9ecead22076ef950
chanjunren <chanjunren@gmail.com>
2021-07-29 15:16:56 +0800

    Implemented basic temi accordion

--- frontend/src/app_manager/dashboards/temi_dashboard/index.js
+++ frontend/src/app_manager/dashboards/temi_dashboard/index.js
@@ -1,18 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import useHttpClient from '../../../company_site/hooks/http_hook';
 import { BASE_ADDRESS } from '../../../util/values';
-import Grid from '@material-ui/core/Grid';
 import Backdrop from '@material-ui/core/Backdrop';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import { makeStyles } from '@material-ui/core/styles';
 import TemiAccordion from './temi_accordion';
 import { withTheme } from '../../../util/theme';
+// import SimpleAccordion from './temi_accordion';
 
 const TemiDashboard = () => {
   const useStyles = makeStyles((theme) => ({
     root: {
       flexGrow: 1,
       padding: 20,
+    },
+    accordionRoot: {
+      width: '100%',
     },
     backdrop: {
       zIndex: theme.zIndex.drawer + 1,
@@ -44,14 +47,19 @@ const TemiDashboard = () => {
       <Backdrop className={classes.backdrop} open={isLoading}>
         <CircularProgress color="inherit" />
       </Backdrop>
-      {/* <Grid container spacing={3}> */}
-        {loadedTemis.map((temi) => {
-          console.log(temi);
-          return <TemiAccordion
-            key={temi.serialNumber}
-            {...temi}/>
+      <div className={classes.accordionRoot}>
+        {loadedTemis.map((temi, index) => {
+          console.log(index);
+          return (
+            <TemiAccordion
+              key={temi.serialNumber}
+              index={index + 1}
+              {...temi}
+            />
+          );
         })}
-      {/* </Grid> */}
+        {/* <SimpleAccordion/> */}
+      </div>
     </div>
   );
 };
--- frontend/src/app_manager/dashboards/temi_dashboard/temi_accordion.js
+++ frontend/src/app_manager/dashboards/temi_dashboard/temi_accordion.js
@@ -1,20 +1,15 @@
 import React from 'react';
-import {
-  Accordion,
-  AccordionActions,
-  AccordionDetails,
-  AccordionSummary,
-  Button,
-  Chip,
-  makeStyles,
-  Typography,
-} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import AccordionActions from '@material-ui/core/AccordionActions';
+import Button from '@material-ui/core/Button';
+import Chip from '@material-ui/core/Chip';
+import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    width: '100%',
-  },
   heading: {
     fontSize: theme.typography.pxToRem(15), //TEST
   },
@@ -33,44 +28,114 @@ const useStyles = makeStyles((theme) => ({
   column: {
     flexBasis: '33.33%',
   },
+  expanded: {
+    "&$expanded": {
+      margin: '4px'
+    }
+  },
 }));
 
 const TemiAccordion = (props) => {
   const classes = useStyles();
 
-  const { serialNumber, owner, applications } = props;
+  const { serialNumber, owner, applications, index } = props;
 
   return (
-    <div className={classes.root}>
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1c-content"
-          id="panel1c-header"
-        >
-          <div className={classes.column}>
-            <Typography className={classes.heading}>{owner}</Typography>
-          </div>
-          <div className={classes.column}>
-            <Typography className={classes.secondaryHeading}>
-              {serialNumber}
-            </Typography>
-          </div>
-        </AccordionSummary>
-        <AccordionDetails>
-          {applications.map((app) => {
-            return <Chip label={app} />;
-          })}
-        </AccordionDetails>
-        <AccordionActions>
-          <Button size="small">Delete</Button>
-          <Button size="small" color="primary">
-            Edit
-          </Button>
-        </AccordionActions>
-      </Accordion>
-    </div>
+    <Accordion>
+      <AccordionSummary
+        expandIcon={<ExpandMoreIcon />}
+        aria-label="Expand"
+        aria-controls={`panel${index}c-content`}
+        id={`panel${index}c-header`}
+      >
+        <div className={classes.column}>
+          <Typography className={classes.heading}>{owner}</Typography>
+        </div>
+        <div className={classes.column}>
+          <Typography className={classes.secondaryHeading}>
+            {serialNumber}
+          </Typography>
+        </div>
+      </AccordionSummary>
+      <AccordionDetails>
+        {applications.map((app) => {
+          return <Chip label={app} />;
+        })}
+      </AccordionDetails>
+      <AccordionActions>
+        <Button size="small">Delete</Button>
+        <Button size="small" color="primary">
+          Edit
+        </Button>
+      </AccordionActions>
+    </Accordion>
   );
 };
 
 export default TemiAccordion;
+
+// import React from 'react';
+// import { makeStyles } from '@material-ui/core/styles';
+// import Accordion from '@material-ui/core/Accordion';
+// import AccordionSummary from '@material-ui/core/AccordionSummary';
+// import AccordionDetails from '@material-ui/core/AccordionDetails';
+// import Typography from '@material-ui/core/Typography';
+// import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     width: '100%',
+//   },
+//   heading: {
+//     fontSize: theme.typography.pxToRem(15),
+//     fontWeight: theme.typography.fontWeightRegular,
+//   },
+// }));
+
+// export default function SimpleAccordion() {
+//   const classes = useStyles();
+
+//   return (
+//     <div className={classes.root}>
+//       <Accordion>
+//         <AccordionSummary
+//           expandIcon={<ExpandMoreIcon />}
+//           // aria-controls="panel1a-content"
+//           // id="panel1a-header"
+//         >
+//           <Typography className={classes.heading}>Accordion 1</Typography>
+//         </AccordionSummary>
+//         <AccordionDetails>
+//           <Typography>
+//             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+//             sit amet blandit leo lobortis eget.
+//           </Typography>
+//         </AccordionDetails>
+//       </Accordion>
+//       <Accordion>
+//         <AccordionSummary
+//           expandIcon={<ExpandMoreIcon />}
+//           // aria-controls="panel2a-content"
+//           // id="panel2a-header"
+//         >
+//           <Typography className={classes.heading}>Accordion 2</Typography>
+//         </AccordionSummary>
+//         <AccordionDetails>
+//           <Typography>
+//             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+//             sit amet blandit leo lobortis eget.
+//           </Typography>
+//         </AccordionDetails>
+//       </Accordion>
+//       <Accordion disabled>
+//         <AccordionSummary
+//           expandIcon={<ExpandMoreIcon />}
+//           // aria-controls="panel3a-content"
+//           // id="panel3a-header"
+//         >
+//           <Typography className={classes.heading}>Disabled Accordion</Typography>
+//         </AccordionSummary>
+//       </Accordion>
+//     </div>
+//   );
+// }
