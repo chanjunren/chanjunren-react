f6fec21f7566b90f9f4fb8115e650efde851f86b
chanjunren <chanjunren@gmail.com>
2021-08-09 13:16:41 +0800

    Touched up sidebar

--- frontend/src/company_site/components/sidebar/index.js
+++ frontend/src/company_site/components/sidebar/index.js
@@ -8,7 +8,25 @@ import {
   SidebarLink,
 } from './sidebar_elements.js';
 
-const SideBar = ({toggle, isOpen}) => {
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import { FaFileExcel } from 'react-icons/fa';
+
+const useStyles = makeStyles((theme) => ({
+  signInButton: {
+    fontSize: '20px',
+    borderRadius: '25px',
+    padding: '20px',
+  },
+  buttonWrapper: {
+    paddingTop: '15px',
+    display: 'flex',
+    justifyContent: 'center',
+  },
+}));
+
+const SideBar = ({ toggle, isOpen }) => {
+  const classes = useStyles();
   return (
     <SidebarContainer isOpen={isOpen}>
       <Icon onClick={toggle}>
@@ -16,13 +34,34 @@ const SideBar = ({toggle, isOpen}) => {
       </Icon>
       <SidebarWrapper>
         <SidebarMenu>
-          <SidebarLink to="about" onClick={toggle}>About</SidebarLink>
-          <SidebarLink to="gallery" onClick={toggle}>Gallery</SidebarLink>
-          <SidebarLink to="applications" onClick={toggle}>Guides</SidebarLink>
-          <SidebarLink to="contactUs" onClick={toggle}>Contact</SidebarLink>
+          <SidebarLink to="about" onClick={toggle}>
+            About
+          </SidebarLink>
+          <SidebarLink to="products" onClick={toggle}>
+            Products
+          </SidebarLink>
+          <SidebarLink to="gallery" onClick={toggle}>
+            Gallery
+          </SidebarLink>
+          <SidebarLink to="apps" onClick={toggle}>
+            Apps
+          </SidebarLink>
+          <SidebarLink to="press" onClick={toggle}>
+            Press
+          </SidebarLink>
+          <SidebarLink to="contactUs" onClick={toggle}>
+            Contact Us
+          </SidebarLink>
         </SidebarMenu>
-        {/* <SidebtnWrap>
-        </SidebtnWrap> */}
+        <div className={classes.buttonWrapper}>
+          <Button
+            className={classes.signInButton}
+            href="/signin"
+            color="secondary"
+          >
+            Sign In
+          </Button>
+        </div>
       </SidebarWrapper>
     </SidebarContainer>
   );
