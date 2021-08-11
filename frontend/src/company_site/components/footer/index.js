40aafbc8818945a0db961671feae2efa0f1c5b86
chanjunren <chanjunren@gmail.com>
2021-08-11 17:51:49 +0800

    Touched up footer

--- frontend/src/company_site/components/footer/index.js
+++ frontend/src/company_site/components/footer/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 import './footer.css';
-import {Link as LinkS} from 'react-router-dom';
-import {FaFacebook, FaInstagram, FaLinkedin} from 'react-icons/fa';
+import { Link as LinkS } from 'react-router-dom';
+import { FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa';
 const Footer = () => {
   return (
     <footer id="footer-container">
@@ -11,57 +11,76 @@ const Footer = () => {
           <div className="footer-links-wrapper">
             <div className="footer-link-items">
               <h1 className="footer-link-title">Robosolutions</h1>
-              <LinkS className="footer-link"
-                to="about">About</LinkS>
-              <LinkS className="footer-link"
-                to="gallery">Gallery</LinkS>
-              <LinkS className="footer-link"
-                to="applications">Applications</LinkS>
-              <LinkS className="footer-link"
-                to="contactUs">Contact Us</LinkS>
+              <LinkS
+                className="footer-link"
+                to="about"
+                smooth={true}
+                duration={500}
+                spy={true}
+                exact="true"
+                offset={-80}
+              >
+                About
+              </LinkS>
+              <LinkS className="footer-link" to="products">
+                Products
+              </LinkS>
+              <LinkS className="footer-link" to="gallery">
+                Gallery
+              </LinkS>
+              <LinkS className="footer-link" to="apps">
+                Applications
+              </LinkS>
+              <LinkS className="footer-link" to="press">
+                Press
+              </LinkS>
+              <LinkS className="footer-link" to="contactUs">
+                Contact Us
+              </LinkS>
             </div>
             <div className="footer-link-items">
               <h1 className="footer-link-title">Temi</h1>
-              <a className="footer-link"
-                href="https://www.instagram.com/roadtomagician/">Official Website </a>
-            </div>
-          </div>
-          <div className="footer-links-wrapper">
-            <div className="footer-link-items">
-              <h1 className="footer-link-title">Social Media</h1>
-              <a className="footer-link"
-                href="https://facebook.com">Instagram</a>
-              <a className="footer-link"
-                href="https://facebook.com">Facebook</a>
-              <a className="footer-link"
-                href="https://facebook.com">Youtube</a>
+              <a
+                className="footer-link"
+                href="https://www.instagram.com/roadtomagician/"
+              >
+                Official Website{' '}
+              </a>
             </div>
           </div>
         </div>
         <section id="social-media">
           <div id="social-media-wrap">
-            <LinkS to="/" id="social-logo">Robosolutions</LinkS>
+            <LinkS to="/" id="social-logo">
+              Robosolutions
+            </LinkS>
             <small id="website-rights">
-              Robosolutions © {new Date().getFullYear()}  All Rights Reserved
+              Robosolutions © {new Date().getFullYear()} All Rights Reserved
             </small>
             <div id="social-icons">
-              <a className="social-icon-link"
-                href="/"
+              <a
+                className="social-icon-link"
+                href="https://www.facebook.com/Temirobosolutions"
                 target="_blank"
-                aria-label="Facebook">
-                <FaFacebook/>
+                aria-label="Facebook"
+              >
+                <FaFacebook />
               </a>
-              <a className="social-icon-link"
-                href="/"
+              <a
+                className="social-icon-link"
+                href="https://www.instagram.com/temi_singapore/"
                 target="_blank"
-                aria-label="Instagram">
-                <FaInstagram/>
+                aria-label="Instagram"
+              >
+                <FaInstagram />
               </a>
-              <a className="social-icon-link"
-                href="/"
+              <a
+                className="social-icon-link"
+                href="https://www.linkedin.com/in/wenchyi-lim-a228637/"
                 target="_blank"
-                aria-label="LinkedIn">
-                <FaLinkedin/>
+                aria-label="LinkedIn"
+              >
+                <FaLinkedin />
               </a>
             </div>
           </div>
