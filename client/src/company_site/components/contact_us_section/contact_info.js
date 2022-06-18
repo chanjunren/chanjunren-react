f8e94d8939cb34604a3ef7307c1fcce3b968bb05
Jun Ren <chanjunren@gmail.com>
2022-06-18 17:26:05 +0800

    Updated contact info

--- client/src/company_site/components/contact_us_section/contact_info.js
+++ client/src/company_site/components/contact_us_section/contact_info.js
@@ -6,15 +6,21 @@ const contacts = [
 		contactNumber: '+(65) 8303 2264'
 	},
 	{
-		country: 'Singapore',
-		address: '10 Ubi Crescent #07-50 Ubi TechPark (Lobby C) Singapore 408564',
-		email: 'enquiries@robosolutions.sg',
-		contactNumber: '+(65) 8303 2264'
+		country: 'Malaysia',
+		address: 'Unit 15-4 Level 4 Galleria Cyberjaya Jalan Teknokrat 6 Cyber 5 Cyberjaya 63000 Selangor, Malaysia',
+		email: 'sales@robopreneur.com',
+		contactNumber: '+60 (0)3 2726 2476'
 	}, {
-		country: 'Singapore',
-		address: '10 Ubi Crescent #07-50 Ubi TechPark (Lobby C) Singapore 408564',
-		email: 'enquiries@robosolutions.sg',
-		contactNumber: '+(65) 8303 2264'
+		country: 'Malaysia',
+		address: '21 Lifestyle Sdn Bhd 29—3A-3 Maritime Persiaran Karpal Singh 2 11600 Georgetown Pulau Pinang, Malaysia',
+		email: 'info@twenty-one.my',
+		contactNumber: '+60 (0)16 643 4495'
+	},
+	{
+		country: 'Indonesia',
+		address: 'PT Esky Multimedia Indonesia Jalan Raya Kelapa Nias HF3 Mahaka Square Ground Floor C/35 Kelapa Gading Jakarta Utara 14250 Indonesia',
+		email: 'harry@temisolutions.id',
+		contactNumber: '+62 (0)816 708 787'
 	},
 ];
 
