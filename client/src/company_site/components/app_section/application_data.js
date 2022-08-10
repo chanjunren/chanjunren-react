27b7b5ac0f44ac09e7d11053f29a40bf941fccfd
j4ck990 <jackng990@gmail.com>
2022-08-10 17:39:38 +0800

    Add temieye and temiwayfinder application

--- client/src/company_site/components/app_section/application_data.js
+++ client/src/company_site/components/app_section/application_data.js
@@ -2,6 +2,8 @@ import temiMuseumIcon from '../../images/app_logos/temi_museum_icon.png';
 import receptemistIcon from '../../images/app_logos/receptemist_icon.png';
 import temiAnnouncerIcon from '../../images/app_logos/temi_announcer_icon.png';
 import temiConciergeIcon from '../../images/app_logos/temi_concierge_icon.png';
+import temiWayFinderIcon from '../../images/app_logos/temi_wayfinder_icon.png';
+import temiEyeIcon from '../../images/app_logos/temi_eye_icon.png';
 
 export const applicationsData = [
   {
@@ -26,6 +28,17 @@ export const applicationsData = [
     title: 'TemiAnnouncer',
     description: 'Announcer Application',
     url: 'https://github.com/temideveloper/Rs-App-Guides/blob/master/TemiAnnouncer/preview.md'
-    
+  },
+  {
+    imageSrc: temiWayFinderIcon,
+    title: 'TemiWayFinder',
+    description: 'WayFinder Application',
+    url: 'https://github.com/temideveloper/Rs-App-Guides/blob/master/TemiWayFinder/preview.md'
+  },
+  {
+    imageSrc: temiEyeIcon,
+    title: 'TemiEye',
+    description: 'Surveillance Application',
+    url: 'https://github.com/temideveloper/Rs-App-Guides/blob/master/TemiEye/README.md'
   },
 ];
